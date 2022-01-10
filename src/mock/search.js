57d9590979bf8d75bfd1bd6b4019ff8d37907fd4
dogmaker <firefactoryan@gmail.com>
2022-01-09 23:07:58 -0300

    Added filter page

--- src/mock/search.js
+++ src/mock/search.js
@@ -31,7 +31,7 @@ const search = [
       'Hello, my name is Hermon. I was born in Belize. I am a native English and Belize Kriol Speaker. I am a retired semi-professional basketball player which I played for 4 years, with 2 championship trophies. Besides basketball, I love reading car magazines and customizing vehicles, mountain biking, swimming, fishing, cooking and travelli ...',
     rattings: 5,
     prices: {
-      valuePerMentoring: 80,
+      valuePerMentoring: 85,
       mentoringPackages: [
         {
           name: 'Oferta mango',
@@ -140,14 +140,14 @@ const search = [
           color: 'danger',
         },
         {
-          skill: 'sql',
+          skill: 'php',
           level: 80,
           mentory: true,
           group: 'data science',
           color: 'danger',
         },
         {
-          skill: 'contract-tests',
+          skill: 'python',
           level: 50,
           mentory: true,
           group: 'tests',
--- src/views/forms/floating-labels/FloatingLabels.js
+++ src/views/forms/floating-labels/FloatingLabels.js
@@ -1,4 +1,4 @@
-import React, { useState } from 'react'
+import React, { useState, useEffect } from 'react'
 import search from '../../../mock/search.js'
 import options from '../../../mock/habilities.js'
 import Select from 'react-select'
@@ -8,23 +8,24 @@ const animatedComponents = makeAnimated()
 
 const FloatingLabels = () => {
   const [selectedOptions, setSelectedOptions] = useState([])
+  const [saco, setSaco] = useState([])
 
   const handleChange = (options) => {
     setSelectedOptions(Array.isArray(options) ? options.map((x) => x.value) : [])
     console.log('Selected Options: ', selectedOptions)
   }
-  const people = [
-    { id: 1, name: 'abc', gender: 'm', age: 15 },
-    { id: 2, name: 'a', gender: 'm', age: 25 },
-    { id: 3, name: 'efg', gender: 'f', age: 5 },
-    { id: 4, name: 'hjk', gender: 'f', age: 35 },
-    { id: 5, name: 'ikly', gender: 'm', age: 41 },
-    { id: 6, name: 'ert', gender: 'f', age: 30 },
-    { id: 7, name: 'qwe', gender: 'f', age: 31 },
-    { id: 8, name: 'bdd', gender: 'm', age: 78 },
-  ]
-  const id_filter = [1, 4, 5, 8]
-  const selected = [50, 80]
+  const selected = [80]
+
+  function filterViaSkills(arr, selected) {
+    return arr.filter((obj) =>
+      selected.every((selected) =>
+        obj.knowledge.skills.some((obj) => obj.skill.includes(selected)),
+      ),
+    )
+  }
+
+  let resp = filterViaSkills(search, selectedOptions)
+  console.log(resp)
 
   return (
     <div>
@@ -36,20 +37,10 @@ const FloatingLabels = () => {
         options={options}
         onChange={handleChange}
       />
-      {people
-        .filter((person) => id_filter.includes(person.id) && person.gender === 'm')
-        .map((filteredPerson) => (
-          <li key={filteredPerson.name} sm="4">
-            {filteredPerson.age + ' ' + filteredPerson.name}
-          </li>
-        ))}
-      {search
-        .filter((search) => selected.includes(search.knowledge.skills.level))
-        .map((filteredPerson) => (
-          <li key={filteredPerson.completeName} sm="4">
-            {filteredPerson.completeName}
-          </li>
-        ))}
+      <p>{'FIltros'}</p>
+      {resp.map((item, index) => (
+        <p key={index}>{item.clientId}</p>
+      ))}
     </div>
   )
 }
